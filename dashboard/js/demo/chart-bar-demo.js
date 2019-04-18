d7f30b10d5142cdd8d8eb371277d2accb14973f7
MANTA MONDEILA KILONE p1411486 <kilone.manta-mondeila@etu.univ-lyon1.fr>
2019-04-18 14:20:00 +0200

    modif js

--- dashboard/js/demo/chart-bar-demo.js
+++ dashboard/js/demo/chart-bar-demo.js
@@ -2,14 +2,100 @@
 Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = '#292b2c';
 
-// Bar Chart Example
+// Bar Chart 1
 var ctx = document.getElementById("myBarChart");
 var myLineChart = new Chart(ctx, {
   type: 'bar',
   data: {
-    labels: ["January", "February", "March", "April", "May", "June"],
+    labels: ["Oignons", "Tomates", "Pâte brisée", "Poivrons", "Cornichons", "Moutarde", "Fromage rapée"],
     datasets: [{
-      label: "Revenue",
+      label: "Ingredient",
+      backgroundColor: "rgba(2,117,216,1)",
+      borderColor: "rgba(2,117,216,1)",
+      data: [4215, 5312, 6251, 7841, 9821, 14984],
+    }],
+  },
+  options: {
+    scales: {
+      xAxes: [{
+        time: {
+          unit: 'month'
+        },
+        gridLines: {
+          display: false
+        },
+        ticks: {
+          maxTicksLimit: 10
+        }
+      }],
+      yAxes: [{
+        ticks: {
+          min: 0,
+          max: 15000,
+          maxTicksLimit: 5
+        },
+        gridLines: {
+          display: true
+        }
+      }],
+    },
+    legend: {
+      display: false
+    }
+  }
+});
+
+// Bar Chart 2
+var ctx = document.getElementById("myBarChart2");
+var myLineChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ["Oignons", "Tomates", "Pâte brisée", "Poive", "Sel", "Jambons de parme", "Basilique", "Oeufs", "Pot de ricotta"],
+    datasets: [{
+      label: "Ingredient",
+      backgroundColor: "rgba(2,117,216,1)",
+      borderColor: "rgba(2,117,216,1)",
+      data: [4215, 5312, 6251, 7841, 9821, 14984],
+    }],
+  },
+  options: {
+    scales: {
+      xAxes: [{
+        time: {
+          unit: 'month'
+        },
+        gridLines: {
+          display: false
+        },
+        ticks: {
+          maxTicksLimit: 12
+        }
+      }],
+      yAxes: [{
+        ticks: {
+          min: 0,
+          max: 15000,
+          maxTicksLimit: 5
+        },
+        gridLines: {
+          display: true
+        }
+      }],
+    },
+    legend: {
+      display: false
+    }
+  }
+});
+
+// Bar Chart 3
+var ctx = document.getElementById("myBarChart4");
+var myLineChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ["Oignons", "Tomates", "Pâte brisée", "Poivrons", "Cornichons", "Moutarde", "Fromage rapée"],
+    datasets: [{
+      label: "Ingredient",
       backgroundColor: "rgba(2,117,216,1)",
       borderColor: "rgba(2,117,216,1)",
       data: [4215, 5312, 6251, 7841, 9821, 14984],
