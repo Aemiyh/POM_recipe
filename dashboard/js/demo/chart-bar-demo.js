24cdcd04d1486f8722125dffc18c07d7450b539e
Emmy <kotorisuzume@gmail.com>
2019-05-28 17:42:56 +0200

    Erreur json + ajustement graphique

--- dashboard/js/demo/chart-bar-demo.js
+++ dashboard/js/demo/chart-bar-demo.js
@@ -90,13 +90,13 @@ window.onload = function() {
                             display: false
                             },
                             ticks: {
-                            maxTicksLimit: 12
+                            maxTicksLimit: 15
                             }
                         }],
                         yAxes: [{
                             ticks: {
                             min: 0,
-                            max: 150,
+                            max: 15,
                             maxTicksLimit: 5
                             },
                             gridLines: {
